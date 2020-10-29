eba095487eaaef4521f775ef57f88a9f62e2bd61
Charles Yang <charles_yang@berkeley.edu>
2020-10-29 21:36:54 +0000

    fix boxes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,9 @@
 class Movie < ActiveRecord::Base
   def self.with_ratings(ratings_list)
-    Movie.where(rating: ratings_list)
+    ["G", "PG", "PG-13", "R"]
   end
   
   def self.with_ratings(ratings_list)
-    ["G", "PG", "PG-13", "R"]
+    Movie.where(rating: ratings_list)
   end
- 
 end
